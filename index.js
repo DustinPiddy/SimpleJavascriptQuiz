b76fc0948d35d0b99864bbe7c711219694fc3640
Dustin Pidcock <dustygen@gmail.com>
2020-10-11 18:07:23 -0700

    made tiny changes to html and started working on the js

---
+++ index.js
@@ -0,0 +1,56 @@
+//Selecting all Elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const qImg = document.getElementById("questionImage");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+
+//Choices
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+
+//Progress and Score
+const progress = document.getElementById("progress");
+const scoreContainer = document.getElementById("scoreContainer");
+
+//Questions
+let questions = [
+{
+    question: "What is the most popular game console?",
+    imgSrc: " ",
+    choiceA:" ",
+    choiceB:" ",
+    choiceC:" ",
+    correct:" "
+},
+{
+    question: "What is the most popular game console?",
+    imgSrc: " ",
+    choiceA:" ",
+    choiceB:" ",
+    choiceC:" ",
+    correct:" "
+},
+{
+    question: "What is the most popular game console?",
+    imgSrc: " ",
+    choiceA:" ",
+    choiceB:" ",
+    choiceC:" ",
+    correct:" "
+},
+
+
+
+
+
+
+
+
+
+
+
+];
